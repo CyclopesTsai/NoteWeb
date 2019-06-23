05ae5fc380d47fab66bcf7de07612c4ee8ac821c
CyclopesTsai <37129167+CyclopesTsai@users.noreply.github.com>
2019-06-24 01:04:38 +0800

    update

--- pages/note/note.js
+++ pages/note/note.js
@@ -1,17 +1,20 @@
-var app = angular.module('workApp', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ui.bootstrap', 'dialogs.main']);                                                                                                                                                                                                                        
-app.controller('workCtrl',function($scope, $http, $cookies, $window, dialogs) {
+var app = angular.module('noteApp', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ui.bootstrap', 'dialogs.main']);                                                                                                                                                                                                                        
+app.controller('noteCtrl',function($scope, $http, $cookies, $window, dialogs) {
 
 	$scope.tmpData = {
 		subject: '',
+		class: '',
 		content: ''
 	};
 
+	$scope.onUpd = false;
+
 	angular.element(document).ready(function () {
-		$scope.getWorkList();
+		$scope.getNoteList();
 	});
 	
-	$scope.getWorkList = function() {
-		var param = 'func=workJson';
+	$scope.getNoteList = function() {
+		var param = 'func=noteWithClassJson';
 		
 		waitingDialog.show();
 		$http({
@@ -19,40 +22,20 @@ app.controller('workCtrl',function($scope, $http, $cookies, $window, dialogs) {
 			url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec?'+param
 		}).then(function (response){
 			//debugger;
-			$scope.workList = response.data;
+			$scope.noteList = response.data.noteList;
+			$scope.classList = response.data.classList;
 			
 			waitingDialog.hide();
 		});
 	}
 	
-	$scope.changeStatus = function(work) {
-		if(work) {
-			var param = 'func=workUpd';
-			param += ('&data=' + encodeURIComponent(angular.toJson(work)));
-			
-			waitingDialog.show();
-			$http({
-				method: 'POST',
-				url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec',
-				data : param,
-				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-			}).then(function (response){
-				if( response.data == 'true' ){
-					work.status = 'Y';
-				}
-				waitingDialog.hide();
-			});
-		}
-	}
-	
-	$scope.insert = function() {
+	$scope.add = function() {
 		if($scope.tmpData) {
-			if(!$scope.tmpData.subject || !$scope.tmpData.content){
+			if(!$scope.tmpData.subject || !$scope.tmpData.class || !$scope.tmpData.content){
 				return;
 			}
 			
-			debugger;
-			var param = 'func=workAdd';
+			var param = 'func=noteAdd';
 			param += ('&data=' + encodeURIComponent(angular.toJson($scope.tmpData)));
 			
 			waitingDialog.show();
@@ -65,20 +48,87 @@ app.controller('workCtrl',function($scope, $http, $cookies, $window, dialogs) {
 				if( response.data == 'true' ){
 					$scope.tmpData = {
 						subject: '',
+						class: '',
 						content: ''
 					};
 				}
 				
-				param = 'func=workJson';
+				param = 'func=noteJson';
 				$http({
 					method: 'GET',
 					url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec?'+param
 				}).then(function (response){
-					$scope.workList = response.data;
+					$scope.noteList = response.data
+					
+					waitingDialog.hide();
 				});
-				
-				waitingDialog.hide();
 			});
 		}
+	}
+	
+	$scope.upd = function(note) {
+		if(note) {
+			var param = 'func=noteUpd';
+			param += ('&data=' + encodeURIComponent(angular.toJson(note)));
+			
+			waitingDialog.show();
+			$http({
+				method: 'POST',
+				url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec',
+				data : param,
+				headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+			}).then(function (response){
+				
+				param = 'func=noteJson';
+				$http({
+					method: 'GET',
+					url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec?'+param
+				}).then(function (response){
+					$scope.noteList = response.data;
+					
+					$scope.onUpd = false;
+					waitingDialog.hide();
+				});
+			});
+		}
+	}
+	
+	$scope.del = function(note) {
+		if(note) {
+			var dlg = dialogs.confirm();
+			dlg.result.then(function(btn) {
+				var param = 'func=noteDel';
+				param += ('&data=' + encodeURIComponent(angular.toJson(note)));
+				
+				waitingDialog.show();
+				$http({
+					method: 'POST',
+					url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec',
+					data : param,
+					headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+				}).then(function (response){
+					
+					param = 'func=noteJson';
+					$http({
+						method: 'GET',
+						url: 'https://script.google.com/macros/s/AKfycbzUuJYOIQ9lwyhbTRtLky_rl0tg-AS0oJtz2YWSSbhwZGROXodQ/exec?'+param
+					}).then(function (response){
+						$scope.noteList = response.data;
+						
+						waitingDialog.hide();
+					});
+				});
+			});
+		}
+	}
+	
+	$scope.doUpd = function(note) {
+		note.onUpd = true;
+		$scope.onUpd = true;
+	}
+	
+	$scope.doCancel = function(note) {
+		note.onUpd = false;
+		$scope.onUpd = false;
 	}
 });
